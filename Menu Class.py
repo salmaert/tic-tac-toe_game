34441e2ee97e12d4a903069d03be19cf4eaa0bc9
salmaert <salma.elibrahimi2002@gmail.com>
2025-05-29 13:06:58 -0400

    Create Menu Class.py

---
+++ Menu Class.py
@@ -0,0 +1,25 @@
+class Menu:
+    def display_main_menu(self):
+        while True:
+            print("welcome to X&0 Game:")
+            print("1. start the game")
+            print("2.quit the game")
+            choice = input("please enter your choice (1 or 2) : ")
+            if choice == "1" or choice == "2":
+                break
+            else:
+                print("please enter 1 or 2")
+        return choice
+
+    def display_endgame_menu(self):
+        while True:
+         menu_text = """ Game over  :
+         1.restart the game
+         2.quit the game     
+         """
+         print(menu_text)
+         choice = input("please enter your choice (1 or 2) : ")
+         if choice == "1" or choice == "2":
+          return choice
+         else :
+          print("please enter 1 or 2 :")
