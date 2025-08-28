7b11edcfd95a57b57185c952f4ba6e0b0afa46ab
salmaert <salma.elibrahimi2002@gmail.com>
2025-08-28 09:24:44 -0400

    Update Menu Class.py

--- Menu Class.py
+++ Menu Class.py
@@ -3,7 +3,7 @@ class Menu:
         while True:
             print("welcome to X&0 Game:")
             print("1. start the game")
-            print("2.quit the game")
+            print("2. quit the game")
             choice = input("please enter your choice (1 or 2) : ")
             if choice == "1" or choice == "2":
                 break
@@ -13,13 +13,13 @@ class Menu:
 
     def display_endgame_menu(self):
         while True:
-         menu_text = """ Game over  :
-         1.restart the game
-         2.quit the game     
-         """
-         print(menu_text)
-         choice = input("please enter your choice (1 or 2) : ")
-         if choice == "1" or choice == "2":
-          return choice
-         else :
-          print("please enter 1 or 2 :")
+            menu_text = """Game over  :
+        1.restart the game
+        2.quit the game     
+        """
+            print(menu_text)
+            choice = input("please enter your choice (1 or 2):")
+            if choice == "1" or choice == "2":
+                break
+            print("please enter 1 or 2 :")
+        return choice
