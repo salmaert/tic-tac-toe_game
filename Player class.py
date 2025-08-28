467edf09fa3aa6acfd385e8292ccd9c90b77c3d0
salmaert <salma.elibrahimi2002@gmail.com>
2025-08-28 09:25:28 -0400

    Update Player class.py

--- Player class.py
+++ Player class.py
@@ -1,22 +1,22 @@
-class Player :
+class Player:
+    def __init__(self):
+        self.name=""
+        self.symbol=""
 
 
- def __init__(self):
-    self.name = ""
-    self.symbol = ""
-
- def choose_name(self):
-    while True:
-        name = input("Please enter your name :(only letters) : ")
-        if name.isalpha():
-            self.name = name
+    def choose_name(self):
+        while True:
+         name=input("Please enter your name :(only letters) : ")
+         if name.isalpha():
+            self.name=name
             break
-    print("invalide name.please use letters only!")
+         print("invalide name.please use letters only!")
 
- def choose_symbol(self):
-    while True:
-        symbol = input("Please enter your symbol :(a single letter) : ")
-        if symbol.isalpha() & len(symbol) == 1:
-            self.symbol = symbol
+    def choose_symbol(self):
+        while True:
+         symbol=input("Please enter your symbol :(a single letter) : ")
+         if symbol.isalpha() & len(symbol) == 1:
+            self.symbol=symbol
             break
-    print("invalide symbol.please use letters only!")
+         print("invalide symbol.please use letters only!")
+
